28f6c4371afea9dcc28a0f51d70287c2371a285b
Joao Marcelo Alves <joao.alves@liferay.com>
2022-05-12 02:25:12 -0300

    Adicao de testes nos utils

--- src/test/kotlin/school/cesar/criptocorretora/util/CPFUtilTest.kt
+++ src/test/kotlin/school/cesar/criptocorretora/util/CPFUtilTest.kt
@@ -1,13 +1,38 @@
 package school.cesar.criptocorretora.util
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class CPFUtilTest {
     private val cpfUtil = CPFUtil();
 
     @Test
-    fun `deve retornar cpf sem simbolos`() {
+    fun `deve limpar cpf`() {
         assertEquals("17154989092", cpfUtil.limpaCPF("171.549.890-92"))
+    }
+
+    @Test
+    fun `deve retornar false se cpf for vazio`() {
+        assertFalse(cpfUtil.isCPF(""))
+    }
+
+    @Test
+    fun `deve retornar false se tamanho de cpf for diferente de 11`() {
+        assertFalse(cpfUtil.isCPF("123"))
+    }
+
+    @Test
+    fun `deve retornar false se os numeros forem repetidos`() {
+        assertFalse(cpfUtil.isCPF("11111111111"))
+    }
+
+    @Test
+    fun `deve retornar false se os digitos finais forem diferente do esperado`() {
+        assertFalse(cpfUtil.isCPF("11122233344"))
+    }
+
+    @Test
+    fun `deve retornar cpf valido`() {
+        assertTrue(cpfUtil.isCPF("171.549.890-92"))
     }
 }
--- src/test/kotlin/school/cesar/criptocorretora/util/SenhaUtilTest.kt
+++ src/test/kotlin/school/cesar/criptocorretora/util/SenhaUtilTest.kt
@@ -7,6 +7,26 @@ internal class SenhaUtilTest {
     private val senhaUtil = SenhaUtil();
 
     @Test
+    fun `deve retornar false se senha nao contem letra minuscula`() {
+        assertFalse(senhaUtil.isFormatoOK("ABC123"))
+    }
+
+    @Test
+    fun `deve retornar false se senha nao contem letra maiuscula`() {
+        assertFalse(senhaUtil.isFormatoOK("abc123"))
+    }
+
+    @Test
+    fun `deve retornar false se senha nao contem numeros`() {
+        assertFalse(senhaUtil.isFormatoOK("abcDEF"))
+    }
+
+    @Test
+    fun `deve retornar senha valida`() {
+        assertTrue(senhaUtil.isFormatoOK("Abcd12345"))
+    }
+
+    @Test
     fun `deve codificar senha`() {
         assertEquals("e8d95a51f3af4a3b134bf6bb680a213a", senhaUtil.md5("senha"))
     }
